80ce8cd1f54c1cb1bc7765fac6bcf0f53d736624
Javed Alam <javedalam96542@gmail.com>
2025-02-20 10:41:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,70 +1,17 @@
-// var num=20;
-// console.log(num);
-// var num;
-// num1=50;
-// console.log(num1);
-// let num1;
-// let a=20;
-// let b=16;
-// let res=a+b;
-// console.log(res);
-// let a=20;
-// let b=16;
-// let res=a*b;
-// console.log(res);
-// let a=20;
-// let b=16;
-// let res=a%b;
-// console.log(res);
-// let a=20;
-// let b=16;
-// let res=a+b;
-// console.log(res);
-// let a="20";
-// let b="20";
-// let res=b==a;
-// console.log(res);
-// let a=20;
-// let b="20";
-// let res= b === a;
-// console.log(res);
-// let a="20";
-// let b=true;
-// let res=b===a;
-// console.log(res);
-// let a="20";
-// let b=true;
-// let res = b != a ;
-// console.log(res);
-// let a=20;
-// let b=50;
-// let res = b >a;
-// console.log(res);
-// let a=20;
-// let b=50;
-// let res = b >= a;
-// console.log(res);
-// let a=20;
-// let b=50;
-// let res = b <= a;
-// console.log(res);
-// let a=20;
-// a = +3;
-// console.log(a);
-// let a=20;
-// a %= 3;
-// console.log(a);
-// let a=2;
-// let b=5;
-//  let res = a+b;
-//  console.log(res);
-//  let num=4567;
-//  let rem;
-//  while(num!=0)
-//  {
-//       rem=num%10;
-//       num=num/10;
-//  }
-//  console.log(rem);
-let a=4567;
-let res=Math.floor(a/100);
+
+//let a=20;
+//post increment
+// console.log(a++,a);
+//Pre increment
+//console.log(++a,a,a++,++a,a,a++,a++,++a,++a);
+//console.log(a--,a,--a,--a,a--,a,a,a,--a,a--,a--);
+//console.log(++a,a,--a,a--,a--,a,a,a--,a++,++a,++a,a++,--a);
+let a=20;
+let b=20;
+// console.log(a>b && b<a);
+// console.log(a === b && b == a);
+// console.log(a !=b && b!=a);
+// console.log(a !==b || a==b);
+// console.log(a ===b || a==b);
+console.log((5===5) ? 'a':'b');
+
