75e97bf4f929f3c47e6a278084ca479cafb20f6c
Javed Alam <javedalam96542@gmail.com>
2025-02-20 10:40:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
-console.log("Hello World");
-var num=20;
-function add{
- var num=30;
-}
-console.log(num);
-
-
-let num1=50;
-let num1=40;
-num1=40;
-
-console.log(num1);
-
-
-const num2=60;
-// num2=80;
-console.log(num2);
+// var num=20;
+// console.log(num);
+// var num;
+// num1=50;
+// console.log(num1);
+// let num1;
+// let a=20;
+// let b=16;
+// let res=a+b;
+// console.log(res);
+// let a=20;
+// let b=16;
+// let res=a*b;
+// console.log(res);
+// let a=20;
+// let b=16;
+// let res=a%b;
+// console.log(res);
+// let a=20;
+// let b=16;
+// let res=a+b;
+// console.log(res);
+// let a="20";
+// let b="20";
+// let res=b==a;
+// console.log(res);
+// let a=20;
+// let b="20";
+// let res= b === a;
+// console.log(res);
+// let a="20";
+// let b=true;
+// let res=b===a;
+// console.log(res);
+// let a="20";
+// let b=true;
+// let res = b != a ;
+// console.log(res);
+// let a=20;
+// let b=50;
+// let res = b >a;
+// console.log(res);
+// let a=20;
+// let b=50;
+// let res = b >= a;
+// console.log(res);
+// let a=20;
+// let b=50;
+// let res = b <= a;
+// console.log(res);
+// let a=20;
+// a = +3;
+// console.log(a);
+// let a=20;
+// a %= 3;
+// console.log(a);
+// let a=2;
+// let b=5;
+//  let res = a+b;
+//  console.log(res);
+//  let num=4567;
+//  let rem;
+//  while(num!=0)
+//  {
+//       rem=num%10;
+//       num=num/10;
+//  }
+//  console.log(rem);
+let a=4567;
+let res=Math.floor(a/100);
